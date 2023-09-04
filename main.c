6e2647926dbc904663c198ad835b71c45b65a421
Chase Killorin <chase.k@metactf.com>
2023-09-04 18:35:45 -0400

    Starting adaptive calculatore

--- main.c
+++ main.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 
-int main(){
+void adaptiveCalculator(){
+
+}
+
+void simpleCalculator(){
 
     char operator;
     double x,y;
     printf("Enter an function (+ - * /): ");
-    scanf("%c",&operator);
+    scanf(" %c",&operator);
     printf("Enter two numbers: ");
     scanf("%lf %lf", &x, &y);
 
@@ -26,4 +30,23 @@ int main(){
             printf("Error: Bad Operand");
 
     }
+
+    return;
+}
+
+int main(){
+
+    int calctype;
+    printf("1. Simple Calculator\n2. Adaptive Calculator\nChoice: ");
+    scanf("%i",&calctype);
+
+    if (calctype == 1){
+        simpleCalculator();
+    } else if (calctype == 2){
+        adaptiveCalculator();
+    } else{
+        printf("Error: Bad option\n");
+    }
+    
+    return 0;
 }
